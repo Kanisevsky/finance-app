b6af552eacc8048d02654189a387c79978d4bcb9
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-25 18:00:42 +0100

    $

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -22,7 +22,7 @@ const SignInPage = () => {
         </div>
       </div>
       <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-        <Image src="./logo.svg" alt="logo" width={85} height={45} />
+        <Image src="/logo.svg" alt="Logo" width={100} height={100} />
       </div>
     </div>
   );
