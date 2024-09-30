edf328e79bd9a79f2acdfb3bbd088d235c103b55
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 19:18:34 +0100

    $

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
 
 type Props = {
   label: string;
@@ -7,7 +9,19 @@ type Props = {
 };
 
 const NavButton = ({ label, href, isActive }: Props) => {
-  return <div>NavButton</div>;
+  return (
+    <Button
+      asChild
+      size="sm"
+      variant="outline"
+      className={cn(
+        'w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition',
+        isActive ? 'bg-white/10 text-white' : 'bg-transparent'
+      )}
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
 };
 
 export default NavButton;
