1be85167f3fa840b3b4cc451d0ad98fc76ed170a
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 17:26:25 +0100

    $

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,13 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { clerkMiddleware } from '@hono/clerk-auth';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
 app.get('/hello', clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorised' });
-  }
-  return c.json({
-    message: 'Hello Next.js!',
-  });
+  return c.json({ message: 'Hello World' });
 });
 
 export const GET = handle(app);
