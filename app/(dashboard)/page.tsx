d93207376ff6f91d80a6d9e559ee6614be74b835
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 20:03:20 +0100

    $

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from '@clerk/nextjs';
-
 export default function Home() {
-  return (
-    <div className="">
-      <UserButton />
-    </div>
-  );
+  return <div className=""></div>;
 }
