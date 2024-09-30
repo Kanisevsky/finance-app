6ef6c1075e4a8e53fbd65759ee98b98d5e522f86
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 19:53:48 +0100

    $

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,11 @@
 'use client';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useMedia } from 'react-use';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import NavButton from './NavButton';
 import { useState } from 'react';
+import { Button } from './ui/button';
+import { Menu } from 'lucide-react';
 const routes = [
   { href: '/', label: 'Overview' },
   { href: '/transactions', label: 'Transactions' },
@@ -13,8 +15,43 @@ const routes = [
 ];
 
 const Navigation = () => {
-  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+  const isMobile = useMedia('(max-width: 1024px)', false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-normal text-white focus:bg-white/30 transition bg-white/10 outline-none hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent"
+          >
+            <Menu className="h-4 w-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname ? 'secondary' : 'ghost'}
+                key={route.href}
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {routes.map((route) => (
