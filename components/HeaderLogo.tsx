717ad8cb73cd48b6f248ff5295c99e71b48f353b
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 18:23:26 +0100

    $

---
+++ components/HeaderLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+const HeaderLogo = () => {
+  return (
+    <Link href="/">
+      <div className="">Logo</div>
+    </Link>
+  );
+};
+
+export default HeaderLogo;
