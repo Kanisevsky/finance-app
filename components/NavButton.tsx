38f46971589a41281b0ee1c6d806968a38febdd4
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-30 18:54:50 +0100

    $

---
+++ components/NavButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+  label: string;
+  href: string;
+  isActive?: boolean;
+};
+
+const NavButton = ({ label, href, isActive }: Props) => {
+  return <div>NavButton</div>;
+};
+
+export default NavButton;
---
+++ components/Navigation.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import NavButton from './NavButton';
+
+const routes = [
+  { href: '/', label: 'Overview' },
+  { href: '/transactions', label: 'Transactions' },
+  { href: '/accounts', label: 'Accounts' },
+  { href: '/categories', label: 'Categories' },
+  { href: '/settings', label: 'Settings' },
+];
+
+const Navigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          label={route.label}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
