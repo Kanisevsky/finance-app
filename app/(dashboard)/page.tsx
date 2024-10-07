ade212e9b39a6417418090d49efb16fe4ee7967d
Kanisevsky <meltsmokestudio@gmail.com>
2024-10-07 19:47:57 +0100

    $

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,3 @@
-'use client';
-
-import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
-
 export default function Home() {
-  const accountsQuery = useGetAccounts();
-  return (
-    <div className="">
-      {accountsQuery.data?.map((account) => (
-        <div key={account.id}>{account.name}</div>
-      ))}
-    </div>
-  );
+  return <div>HomePage</div>;
 }
