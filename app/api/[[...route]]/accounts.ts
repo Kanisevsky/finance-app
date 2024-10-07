02c7a24680c8d071a5a95fd2d94c82f330cd8935
Kanisevsky <meltsmokestudio@gmail.com>
2024-10-07 19:27:08 +0100

    $

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,16 @@
+import { db } from '@/db/drizzle';
 import { Hono } from 'hono';
+import { accounts } from '@/db/schema';
 
-const app = new Hono().get('/', (c) => {
-  return c.json({ accounts: [] });
+const app = new Hono().get('/', async (c) => {
+  const data = await db
+    .select({
+      id: accounts.id,
+      name: accounts.name,
+    })
+    .from(accounts);
+
+  return c.json({ data });
 });
 
 export default app;
