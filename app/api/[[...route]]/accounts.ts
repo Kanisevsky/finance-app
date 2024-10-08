3ae265fa19f4ebbce4edbda6fe2c31228590ce11
Kanisevsky <meltsmokestudio@gmail.com>
2024-10-08 15:02:27 +0100

    $

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,16 +1,13 @@
 import { db } from '@/db/drizzle';
 import { Hono } from 'hono';
 import { eq } from 'drizzle-orm';
-import { HTTPException } from 'hono/http-exception';
 import { accounts } from '@/db/schema';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
 const app = new Hono().get('/', clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
   if (!auth?.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: 'Unauthorised' }, 401),
-    });
+    return c.json({ error: 'Unauthorised' }, 401);
   }
   const data = await db
     .select({
