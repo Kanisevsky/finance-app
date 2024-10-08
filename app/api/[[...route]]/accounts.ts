fc0222b9269da110e78b3b9ff5ad64179b1fefb7
Kanisevsky <meltsmokestudio@gmail.com>
2024-10-08 15:15:33 +0100

    $

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,23 +1,37 @@
 import { db } from '@/db/drizzle';
 import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
 import { eq } from 'drizzle-orm';
-import { accounts } from '@/db/schema';
+import { accounts, insertAccountSchema } from '@/db/schema';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorised' }, 401);
-  }
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId));
+const app = new Hono()
+  .get('/', clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: 'Unauthorised' }, 401);
+    }
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-  return c.json({ data });
-});
+    return c.json({ data });
+  })
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator('json', insertAccountSchema),
+    async (c) => {
+      const auth = getAuth(c);
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorised' }, 401);
+      }
+      return c.json({});
+    }
+  );
 
 export default app;
